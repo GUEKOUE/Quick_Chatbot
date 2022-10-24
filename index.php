c1c36c5a84e6bd266ba4b9ebae849339bbd22fe0
GUEKOUE <compassiondemenou@yahoo.com>
2022-10-24 04:56:38 +0100

    last update

--- index.php
+++ index.php
@@ -8,13 +8,18 @@
     <!--================== FAVICON ========================-->
     <link rel="shortcut icon" href="img/bot.png" type="image/x-icon">
 
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css"/>
+    <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
+
     <!--================== CSS ========================-->  
     <link rel="stylesheet" href="bot.css">
-    <title>Chatbot</title>
+    <title>Simple | Chatbot</title>
 </head>
 <body>
     
     <div id="bot">
+        <div id="con1"></div>
+        <div id="con2"></div>
         <div id="container">
             <div id="header">
                 Online Chatbot App
@@ -22,7 +27,7 @@
 
             <div id="body">
                 <!-- This section will be dynamically inserted from JavaScript -->
-                <div class="userSection">
+                <!--<div class="userSection">
                     <div class="messages user-message">
 
                     </div>
@@ -34,37 +39,51 @@
 
                     </div>
                     <div class="seperator"></div>
-                </div>                
+                </div>   -->             
             </div>
 
+            <!-- messages input field -->
             <div id="inputArea">
-                <input type="text" name="messages" id="userInput" placeholder="Please enter your message here..." required>
-                <input type="submit" id="send" value="Send">
+                <input type="text" name="messages" id="userInput" placeholder="Enter your message here..." required>
+                <input type="submit" id="send" value="Send" name="send">
             </div>
         </div>
     </div>    
 
     <script type="text/javascript">
 
-        document.querySelector("#send").addEventListener("click", async () => {
+        $(document).ready(function(){
 
-            let xhr = new XMLHttpRequest();
-            var userMessage = document.querySelector("#userInput").value
+            $("#userInput").on("keyup",function(){
 
-            let userHtml = '<div class="userSection">'+'<div class="messages user-message">'+userMessage+'</div>'+'<div class="seperator"></div>'+'</div>'
+                if($("#userInput").val()){
+                    $("#send").css("display","block");
+                }else{
+                    $("#send").css("display","none");
+                }
+            });
+        });
 
-            document.querySelector('#body').innerHTML+= userHtml;
-
-            xhr.open("POST", "query.php");
-            xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
-            xhr.send(`messageValue=${userMessage}`);
-
-            xhr.onload = function () {
-                let botHtml = '<div class="botSection">'+'<div class="messages bot-reply">'+this.responseText+'</div>'+'<div class="seperator"></div>'+'</div>'
-
-                document.querySelector('#body').innerHTML+= botHtml;
-            }
-        })
+            $("#send").on("click", function(){
+                $value = $("#userInput").val();
+                $msg = '<div class="userSection">'+'<div class="messages user-message">'+ $value +'</div>'+'<div class="seperator"></div>'+'</div>';
+                $("#body").append($msg);
+                
+                // start ajax code
+                $.ajax({
+                    url: 'query.php',
+                    type: 'POST',
+                    data: 'messageValue='+$value,
+                    success: function(result){
+                        $replay = '<div class="botSection">'+'<div class="messages bot-reply">'+ result +'</div>'+'<div class="seperator"></div>'+'</div>';
+                        $("#body").append($replay);
+                        // when chat goes down the scroll bar automatically comes to the bottom
+                        $("#body").scrollTop($("#body")[0].scrollHeight);
+                    }
+                });
+                $("#userInput").val("");
+                $("#send").css("display","none");
+            });
     </script>
 </body>
 </html>
